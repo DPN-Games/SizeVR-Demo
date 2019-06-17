b3d396107e7bbbc3da3ae67d657b137c31249090
DigiDuncan <digiduncan@gmail.com>
2019-06-17 16:43:52 -0400

    Nova's initial Unity project.
    Co-Authored-By: Tyler <itsnovahere@users.noreply.github.com>

---
+++ Assets/Scale.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Serialization;
+using Valve.VR;
+
+public class Scale : MonoBehaviour {
+    private float size = 1f;
+	private float prevSize = 0f;
+
+    private const float min = 0.1f;
+    private const float max = 100f;
+
+    public TextMeshPro text;
+    
+    void Start() {
+        var vec2 = SteamVR_Input.GetVector2Action("scaleslider", false);
+        vec2.onAxis += Vec2OnAxis;
+    }
+
+    private void Vec2OnAxis(SteamVR_Action_Vector2 fromaction, SteamVR_Input_Sources fromsource, Vector2 axis, Vector2 delta) {
+        size += axis.y * Time.deltaTime * 5 * (size / 10);
+    }
+
+    void Update() {
+        if (Math.Abs(size - prevSize) > 0) {
+            if (size < min) size = min;
+            if (size > max) size = max;
+            
+            transform.localScale = new Vector3(size * 10, size * 10, size * 10);
+
+            prevSize = size;
+
+            text.text = size.ToString("F2");
+        }
+    }
+}
---
+++ Assets/ScaleNonVR.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ScaleNonVR : MonoBehaviour {
+    private float size = 1f;
+    private float prevSize = 0f;
+
+    private const float min = 0.1f;
+    private const float max = 100f;
+
+    public TextMeshPro text;
+
+    void Update() {
+        if (Input.GetKey(KeyCode.R)) {
+            size += 0.1f;
+        } else if (Input.GetKey(KeyCode.F)) {
+            size -= 0.1f;
+        }
+        
+        if (Math.Abs(size - prevSize) > 0) {
+            if (size < min) size = min;
+            if (size > max) size = max;
+
+            transform.localScale = new Vector3(size * 10, size * 10, size * 10);
+
+            prevSize = size;
+
+            text.text = size.ToString("F2");  
+        }
+    }
+}
